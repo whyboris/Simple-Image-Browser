e62117aac335261b4d37de0e9e74a0e4ce18b0dc
whyboris <yboris@yahoo.com>
2021-04-18 21:52:46 -0400

    temp buttons

--- src/app/home/settings-buttons.ts
+++ src/app/home/settings-buttons.ts
@@ -1,22 +1,44 @@
 import { SettingsButton } from '../../interfaces/settings-object.interface';
 
-export type SettingsButtonKey = 'a'
- | 'b'
- | 'd'
- | 'c';
+export type SettingsButtonKey = 'folder'
+ | 'hideTree'
+ | 'autoHide'
+ | 'view1'
+ | 'view2'
+ | 'view3'
+ | 'view4'
+ | 'zoomIn'
+ | 'zoomOut'
+ | 'jpg'
+ | 'png'
+ | 'gif'
+ | 'view5';
 
 // Add `SettingsButtons` items here so they show up in the buttons ribbon and in the settings
 // Each array separates buttons into their own button groups visually
 export const SettingsButtonsGroups: SettingsButtonKey[][] = [
-  [ // 0 - Search & filter settings
-    'a',
-    'd',
+  [ // 0 - open folder
+    'folder',
   ],
-  [ // 1 - Search filters
-    'b',
+  [ // 1 - show/hide sidebar
+    'hideTree',
+    'autoHide',
   ],
-  [ // 2 - Filters & sorting options
-    'c',
+  [ // 2 - gallery views
+    'view1',
+    'view2',
+    'view3',
+    'view4',
+    'view5',
+  ],
+  [ // 3 -- image size
+    'zoomIn',
+    'zoomOut'
+  ],
+  [ // 4 -- file formats
+    'jpg',
+    'png',
+    'gif'
   ]
 ];
 
@@ -26,15 +48,21 @@ export const SettingsMetaGroup: string[][] = [
     ...SettingsButtonsGroups[0],
     'break',
     ...SettingsButtonsGroups[1],
+    'break',
+    ...SettingsButtonsGroups[2],
+    'break',
+    ...SettingsButtonsGroups[3],
+    'break',
+    ...SettingsButtonsGroups[4],
   ],
   [
-    ...SettingsButtonsGroups[2],
+// second tab buttons!
   ]
 ];
 
 // correspond to each group (tab) above
 export const SettingsMetaGroupLabels: string[] = [
-  'SETTINGS.a',
+  'SETTINGS.ribbon',
   'SETTINGS.b',
   'SETTINGS.c',
 ];
@@ -42,36 +70,108 @@ export const SettingsMetaGroupLabels: string[] = [
 export type SettingsButtonsType = { [key in SettingsButtonKey]: SettingsButton };
 
 export const SettingsButtons: SettingsButtonsType = {
-  'a': {
-    description: 'BUTTONS.a',
+  'folder': {
+    description: 'BUTTONS.folder',
     hidden: false,
-    iconName: 'icon-gear',
-    moreInfo: 'BUTTONS.a',
-    hoverText: 'BUTTONS.a',
-    toggled: true
-  },
-  'b': {
-    description: 'BUTTONS.b',
-    hidden: false,
-    iconName: 'icon-close',
-    moreInfo: 'BUTTONS.b',
-    hoverText: 'BUTTONS.b',
+    iconName: 'icon-folder',
+    moreInfo: 'BUTTONS.folderInfo',
+    hoverText: 'BUTTONS.folderHover',
     toggled: false
   },
-  'c': {
-    description: 'BUTTONS.c',
+  'hideTree': {
+    description: 'BUTTONS.hideTree',
     hidden: false,
-    iconName: 'icon-show-more-info',
-    moreInfo: 'BUTTONS.c',
-    hoverText: 'BUTTONS.c',
+    iconName: 'icon-chevron-left',
+    moreInfo: 'BUTTONS.hideTreeInfo',
+    hoverText: 'BUTTONS.hideTreeHover',
     toggled: false
   },
-  'd': {
-    description: 'BUTTONS.d',
+  'autoHide': {
+    description: 'BUTTONS.autoHide',
     hidden: false,
-    iconName: 'icon-gear',
-    moreInfo: 'BUTTONS.d',
-    hoverText: 'BUTTONS.d',
+    iconName: 'icon-eye-closed',
+    moreInfo: 'BUTTONS.autoHideInfo',
+    hoverText: 'BUTTONS.autoHideHover',
+    toggled: false
+  },
+  'view1': {
+    description: 'BUTTONS.view1',
+    hidden: false,
+    iconName: 'icon-temp1',
+    moreInfo: 'BUTTONS.view1Info',
+    hoverText: 'BUTTONS.view1Hover',
+    toggled: false
+  },
+  'view2': {
+    description: 'BUTTONS.view2',
+    hidden: false,
+    iconName: 'icon-temp2',
+    moreInfo: 'BUTTONS.view2Info',
+    hoverText: 'BUTTONS.view2Hover',
+    toggled: false
+  },
+  'view3': {
+    description: 'BUTTONS.view3',
+    hidden: false,
+    iconName: 'icon-temp3',
+    moreInfo: 'BUTTONS.view3Info',
+    hoverText: 'BUTTONS.view3Hover',
+    toggled: false
+  },
+  'view4': {
+    description: 'BUTTONS.view4',
+    hidden: false,
+    iconName: 'icon-temp4',
+    moreInfo: 'BUTTONS.view4Info',
+    hoverText: 'BUTTONS.view4Hover',
+    toggled: false
+  },
+  'view5': {
+    description: 'BUTTONS.view5',
+    hidden: false,
+    iconName: 'icon-temp5',
+    moreInfo: 'BUTTONS.view5Info',
+    hoverText: 'BUTTONS.view5Hover',
+    toggled: false
+  },
+  'zoomIn': {
+    description: 'BUTTONS.zoomIn',
+    hidden: false,
+    iconName: 'icon-plus',
+    moreInfo: 'BUTTONS.zoomInInfo',
+    hoverText: 'BUTTONS.zoomInHover',
+    toggled: false
+  },
+  'zoomOut': {
+    description: 'BUTTONS.zoomOut',
+    hidden: false,
+    iconName: 'icon-minus',
+    moreInfo: 'BUTTONS.zoomOutInfo',
+    hoverText: 'BUTTONS.zoomOutHover',
+    toggled: false
+  },
+  'jpg': {
+    description: 'BUTTONS.jpg',
+    hidden: false,
+    iconName: 'icon-jpg',
+    moreInfo: 'BUTTONS.jpgInfo',
+    hoverText: 'BUTTONS.jpgHover',
+    toggled: false
+  },
+  'png': {
+    description: 'BUTTONS.png',
+    hidden: false,
+    iconName: 'icon-png',
+    moreInfo: 'BUTTONS.pngInfo',
+    hoverText: 'BUTTONS.pngHover',
+    toggled: false
+  },
+  'gif': {
+    description: 'BUTTONS.gif',
+    hidden: false,
+    iconName: 'icon-gif',
+    moreInfo: 'BUTTONS.gifInfo',
+    hoverText: 'BUTTONS.gifHover',
     toggled: false
   },
 }
